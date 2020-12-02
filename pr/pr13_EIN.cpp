5f79a9b38e905d9fbd5306470d84831fdd84183b
Илья Евсеенко <addminuser258@gmail.com>
2020-12-02 16:48:20 +0300

    буквы вместо цифр

--- pr/pr13_EIN.cpp
+++ pr/pr13_EIN.cpp
@@ -7,20 +7,20 @@ void main( void )
 {
 	page_1();
 	window(1, 1, 80, 25); textattr(15 + (7 << 4)); clrscr();
-	int i; char n; int _time;
+	char i; char n; int _time;
 m:
 	Wind(4, 6, 29, 16, 0, 1);
 	gotoxy(2, 2); cout << "Тарифные планы";
-	gotoxy(2, 4); cout << "1 - Внутри сети";
-	gotoxy(2, 5); cout << "2 - Другие операторы";
-	gotoxy(2, 6); cout << "3 - На городские номера";
+	gotoxy(2, 4); cout << "в - Внутри сети";
+	gotoxy(2, 5); cout << "д - Другие операторы";
+	gotoxy(2, 6); cout << "г - На городские номера";
     gotoxy(1, 8); cout << "Время созвона(сек) => "; cin >> _time;
 	gotoxy(1, 10); cout << "Выбери номер тарифа => "; cin >> i;
 	switch(i)
 	{
-		case  1: _in(_time); break;
-		case  2: _other(_time); break;
-		case  3: _gor_number(_time); break;
+		case  'в': _in(_time); break;
+		case  'д': _other(_time); break;
+		case  'г': _gor_number(_time); break;
 		default: gotoxy(2, 10); cout << "Ошибка"; break;
 	}
 	Wind(4, 19, 27, 21, 0, 18);
